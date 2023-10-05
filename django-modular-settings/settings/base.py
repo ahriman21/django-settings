113261ea8fb06d2342df7249a4c9cc4060556c54
ahriman <109135045+ahriman21@users.noreply.github.com>
2023-10-05 20:15:01 +0330

    Update base.py

--- django-modular-settings/settings/base.py
+++ django-modular-settings/settings/base.py
@@ -30,3 +30,34 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'mysite.urls'
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
+
+AUTH_PASSWORD_VALIDATORS =[
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 9,
+        }
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+# STATIC files
+STATICFILES_DIRS = [
+    # BASE_DIR / 'static',
+    os.path.join(BASE_DIR, 'static') 
+    
+]  # staticfiles_dirs --> is the list of folders where Django will search for additional static files aside from the static folder of each app installed.(development)
+STATIC_URL = 'static/'  # --> the url that uses in terminal 
+
+
+# MEDIA files 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # -->  is the folder where files uploaded using FileField will go.
+MEDIA_URL = '/media/' # --> The MEDIA_URL is the URL that makes the  media accessible over HTTP.
