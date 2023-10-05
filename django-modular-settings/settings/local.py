b014d94afdcec1b434fbf09a9e355704d255824a
ahriman <109135045+ahriman21@users.noreply.github.com>
2023-10-05 20:17:20 +0330

    Update local.py

--- django-modular-settings/settings/local.py
+++ django-modular-settings/settings/local.py
@@ -15,3 +15,10 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# STATIC files
+STATICFILES_DIRS = [
+    # BASE_DIR / 'static',
+    os.path.join(BASE_DIR, 'static') 
+    
+]  # staticfiles_dirs --> is the list of folders where Django will search for additional static files aside from the static folder of each app installed.(development)
